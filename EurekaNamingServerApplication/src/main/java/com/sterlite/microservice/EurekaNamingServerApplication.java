b018c4a0a41901963837d242aaa36b9dd1b28ddf
Dharmaraj Pawale <dharmaraj.pawale@gmail.com>
2020-10-26 14:04:05 +0530

    Added Eureka Naming Server App

---
+++ EurekaNamingServerApplication/src/main/java/com/sterlite/microservice/EurekaNamingServerApplication.java
@@ -0,0 +1,20 @@
+package com.sterlite.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+/**
+ * @author dharmaraj.pawale@sterlite.com
+ * @creation_date 12th-Sep-2020
+ * @version 1.0
+ * @copyright Sterlite Technologies Ltd.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaNamingServerApplication.class, args);
+	}
+
+}
