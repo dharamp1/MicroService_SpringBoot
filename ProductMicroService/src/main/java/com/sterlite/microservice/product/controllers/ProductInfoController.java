03a12213e30a45c7822f298b6881f817762c0636
Dharmaraj Pawale <dharmaraj.pawale@gmail.com>
2021-02-19 21:14:36 +0530

    Added RESTful Web Service methods such as add, update, delete, etc in ProductInfoController Added Column Names in data.sql

--- ProductMicroService/src/main/java/com/sterlite/microservice/product/controllers/ProductInfoController.java
+++ ProductMicroService/src/main/java/com/sterlite/microservice/product/controllers/ProductInfoController.java
@@ -1,10 +1,18 @@
 package com.sterlite.microservice.product.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sterlite.microservice.product.beans.ProductServiceInfo;
@@ -35,4 +43,40 @@ public class ProductInfoController {
 		
 		return new ProductServiceInfo(product, serverPort);
 	}
+	
+	@CrossOrigin
+	@GetMapping
+	public List<Product> getProducts() {
+		return productRepository.findAll();
+	}
+	
+	@CrossOrigin
+	@GetMapping("/{id}")
+	public Product getProductById(@PathVariable long id) {
+		return productRepository.findById(id).orElseThrow(()->new ProductNotFoundException("Sorry! Product with id " + id + " not found"));
+	}
+	
+	@CrossOrigin
+	@PostMapping
+	public @ResponseBody Product addProduct(@RequestBody Product product) {
+		return productRepository.save(product);
+	}
+	
+	@CrossOrigin
+	@PutMapping
+	public @ResponseBody Product updateProductById(@RequestBody Product product) {
+		System.out.println("update called " + product);
+		return productRepository.save(product);
+	}
+	
+	@CrossOrigin
+	@DeleteMapping("/{id}")
+	public @ResponseBody void removeProductById(@PathVariable long id) {
+		System.out.println("delete called");
+		Product product = productRepository.findById(id).orElseThrow(()->new ProductNotFoundException("Sorry! Product with id " + id + " not found"));
+		productRepository.deleteById(product.getId());
+	}
+	
+	
+	
 }
